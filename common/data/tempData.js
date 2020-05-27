81519d896a3540e347b4fe3b361689e2afbb7a9a
Tafadzwa <tgmakuyana@hotmail.com>
2020-05-27 22:06:07 +0700

    main page initial design

---
+++ common/data/tempData.js
@@ -0,0 +1,62 @@
+// api test data
+
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Check Passprot",
+                completed: false
+            },
+            {
+                title: "Book a Room",
+                completed: false
+            },
+            {
+                title: "Pack Laggage",
+                completed: true
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Make a Video",
+                completed: false
+            },
+            {
+                title: "Book a Room",
+                completed: false
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Get Ballons",
+                completed: true
+            },
+            {
+                title: "Send Invitations",
+                completed: false
+            }
+        ]
+    }
+]
