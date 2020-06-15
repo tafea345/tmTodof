acbb4a03c5d6f399977f0ef1f5a9739a9d101dc8
Taf <taf@bitkub.com>
2020-06-15 18:14:57 +0700

    login screen

---
+++ screens/Login/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class loginScreen extends Component {
+    render() {
+        return (
+            <div>
+                Login Page
+            </div>
+        )
+    }
+}
